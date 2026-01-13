977d17ef5303a13f7d55a4c2d62d0468fa35b265
alidanish8065 <134804501+alidanish8065@users.noreply.github.com>
2026-01-13 16:48:25 +0500

    Delete WPL_LAB/project directory

--- WPL_LAB/project/js/script.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const sidebarToggle = document.getElementById('sidebarToggle');
-const pageWrapper = document.getElementById('page-content-wrapper');
-
-sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-    pageWrapper.classList.toggle('full');
-});
--- WPL_LAB/project/tests.php
+++
@@ -1,3 +0,0 @@
-<?php
-require "dbconfig.php";
-echo "Config loaded successfully!";
